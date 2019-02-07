64ccb9b1df2f80b20754c39be68443c00985e142
stvmachine <campos.esteban@gmail.com>
2019-02-07 14:31:19 +1100

    Update fetch_success.js

--- src/fetch/__fixtures__/Users/fetch_success.js
+++ src/fetch/__fixtures__/Users/fetch_success.js
@@ -2,5 +2,23 @@ import Users from '../../components/Users';
 
 export default {
     component: Users,
-    
+    fetch: [
+      {
+        matcher: '/users',
+        response: [
+          {
+            id: 1,
+            name: 'Prabu'
+          },
+          {
+            id: 2,
+            name: 'Karla'
+          },
+          {
+            id: 3,
+            name: 'Linbaba'
+          }
+        ]
+      }
+    ]
   };
