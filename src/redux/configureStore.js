8afc6b6ca4de078cfad15d99459a84e455b8ab68
stevmachine <campos.esteban@gmail.com>
2019-02-23 22:14:05 +1100

    Removing redux devtools on production

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,13 +1,17 @@
-import { createStore, combineReducers } from 'redux';
-import { reducer as reduxFormReducer } from 'redux-form';
-import { devToolsEnhancer } from 'redux-devtools-extension';
+import { createStore, combineReducers } from "redux";
+import { reducer as reduxFormReducer } from "redux-form";
+import { devToolsEnhancer } from "redux-devtools-extension";
 
 const reducer = combineReducers({
   form: reduxFormReducer // mounted under "form"
 });
 
 export default function(initialState) {
-  const store = createStore(reducer, initialState, devToolsEnhancer());
+  const store = createStore(
+    reducer,
+    initialState,
+    process.env.NODE_ENV !== "production" && devToolsEnhancer()
+  );
 
   return store;
 }
